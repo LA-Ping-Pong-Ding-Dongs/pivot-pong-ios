1ec8d6a22ce7cf5ec81156141352a465cd55b73e
Mariana Lenetis and Patrick Collins <pair+mariana+patrick@pivotallabs.com>
2014-04-14 10:22:06 -0700

    XCode project created

---
+++ PivotPong/PivotPong/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PivotPong
+//
+//  Created by pivotal on 4/14/14.
+//  Copyright (c) 2014 Pivotal Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PivotPong/PivotPongTests/PivotPongTests.m
@@ -0,0 +1,34 @@
+//
+//  PivotPongTests.m
+//  PivotPongTests
+//
+//  Created by pivotal on 4/14/14.
+//  Copyright (c) 2014 Pivotal Labs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PivotPongTests : XCTestCase
+
+@end
+
+@implementation PivotPongTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
