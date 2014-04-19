b5148005da6296280e5a10917a58c6088049f0fe
Ehren Murdick <pair+ehren@pivotallabs.com>
2014-04-18 17:50:14 -0700

    set random title on win/loss buttons

--- PivotPong/Controllers/HomeController.m
+++ PivotPong/Controllers/HomeController.m
@@ -4,6 +4,8 @@
 #import "Configurator.h"
 
 @interface HomeController ()
+@property (weak, nonatomic) IBOutlet UIButton *wonButton;
+@property (weak, nonatomic) IBOutlet UIButton *lostButton;
 @end
 
 @implementation HomeController
@@ -11,6 +13,7 @@
 
 
 -(void)viewDidLoad {
+    [self labelButtons];
     if (![[NSUserDefaults standardUserDefaults] objectForKey:@"currentUser"]) {
         [self performSegueWithIdentifier:@"presentIdentification" sender:nil];
     }
@@ -22,6 +25,26 @@
         destinationController = [((UINavigationController *)destinationController) topViewController];
     }
     ((id<Injectable>)destinationController).injector = self.injector;
+}
+
+-(void)labelButtons {
+    NSArray *wonValues = @[
+                           @"I WON!",
+                           @"Winning!",
+                           @"I sucked less"
+                           ];
+    NSArray *lostValues = @[
+                            @"I need to play harder",
+                            @"I sucked",
+                            @"I have some room for improvement",
+                            @"I lost… :-("
+                            ];
+
+    NSString *wonString = [wonValues objectAtIndex:arc4random_uniform(wonValues.count)];
+    [self.wonButton setTitle:wonString forState:UIControlStateNormal];
+
+    NSString *lostString = [lostValues objectAtIndex:arc4random_uniform(lostValues.count)];
+    [self.lostButton setTitle:lostString forState:UIControlStateNormal];
 }
 
 @end
